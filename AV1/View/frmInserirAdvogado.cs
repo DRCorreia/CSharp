754ed440bdb5fd0eb64d58fcd500e736746631e6
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-13 15:44:11 -0300

    Update frmInserirAdvogado.cs

--- AV1/View/frmInserirAdvogado.cs
+++ AV1/View/frmInserirAdvogado.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Model;
+using Controller1;
 
 namespace View
 {
@@ -19,6 +21,16 @@ namespace View
 
         private void btnCadastrar_Click(object sender, EventArgs e)
         {
+           Advogado a = new Advogado();
+            a.Id = txbCpf;
+            a.Nome = txbNome;
+            a.Email = txbEmail;
+            a.Telefone = txbTelefone;
+
+            AdvogadoController ctrlAdv = AdvogadoController();
+
+            ctrlAdv.ExecutarOpBD('i', a);
+
             this.Close();
         }
     }
