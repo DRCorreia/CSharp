a90a745fe00b97e2b31201743d1e3770b5da25ab
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-13 14:37:33 -0300

    Update frmListarAdvogado.cs

--- AV1/View/frmListarAdvogado.cs
+++ AV1/View/frmListarAdvogado.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,43 +13,18 @@ namespace View
 {
     public partial class frmListarAdvogado : Form
     {
-        private Advogado a;
         public frmListarAdvogado()
         {
             InitializeComponent();
         }
-        
-        
-        private void documento_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+
+        private void CarregarMapaAdvogados()
         {
-            StringBuilder data = new StringBuilder();
-            StringWriter escritor = new StringWriter(data);
-
-            Advogado _advogado = a;
-
-            escritor.WriteLine(_advogado.Id + ";");
-            escritor.WriteLine(_advogado.Nome + ";");
-            escritor.WriteLine(_advogado.Email + ";");
-            escritor.WriteLine(_advogado.Telefone + ";");
-
-            escritor.WriteLine(String.Format("\n\n\nData/Hora do Cadastro: {0}", DateTime.Now.ToString()));
-
-            escritor.Close();
-
-            float leftMargin = e.MarginBounds.Left;
-            float topMargin = e.MarginBounds.Top;
-            float yPos = 0;
-
-            Font printFont = new Font("Arial", 12);
-            yPos = topMargin + printFont.GetHeight(e.Graphics);
-            e.HasMorePages = false;
-            e.Graphics.DrawString(data.ToString(), printFont, Brushes.Black, leftMargin, yPos, new StringFormat());
-        }
-
-
-        private void dgvAdvogado_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
+            Dictionary<Int64, Advogado> mapaAdvogados = (Dictionary<Int64, Advogado>)this.Tag;
+            foreach (Pessoa o in mapaAdvogados.Values)
+            {
+                dgvAdvogado.Rows.Add(o.Id, o.Nome, o.Email,o.Telefone);
+            }
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
