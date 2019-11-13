327061f6387d49a54fb0d910bd47dd6f5b1d5147
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-13 13:58:05 -0300

    Update frmListarAdvogado.cs

--- AV1/View/frmListarAdvogado.cs
+++ AV1/View/frmListarAdvogado.cs
@@ -12,10 +12,39 @@ namespace View
 {
     public partial class frmListarAdvogado : Form
     {
+        private Advogado a;
         public frmListarAdvogado()
         {
             InitializeComponent();
         }
+        
+        
+        private void documento_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            StringBuilder data = new StringBuilder();
+            StringWriter escritor = new StringWriter(data);
+
+            Advogado _advogado = a;
+
+            escritor.WriteLine(_advogado.Id + ";");
+            escritor.WriteLine(_advogado.Nome + ";");
+            escritor.WriteLine(_advogado.Email + ";");
+            escritor.WriteLine(_advogado.Telefone + ";");
+
+            escritor.WriteLine(String.Format("\n\n\nData/Hora do Cadastro: {0}", DateTime.Now.ToString()));
+
+            escritor.Close();
+
+            float leftMargin = e.MarginBounds.Left;
+            float topMargin = e.MarginBounds.Top;
+            float yPos = 0;
+
+            Font printFont = new Font("Arial", 12);
+            yPos = topMargin + printFont.GetHeight(e.Graphics);
+            e.HasMorePages = false;
+            e.Graphics.DrawString(data.ToString(), printFont, Brushes.Black, leftMargin, yPos, new StringFormat());
+        }
+
 
         private void dgvAdvogado_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
