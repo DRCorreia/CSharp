725d8f66da3f9059372b7dc68cb1b067697208b1
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-13 14:47:32 -0300

    Add files via upload

---
+++ AV1/Controller1/AdvogadoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controller1
+{
+    class AdvogadoController
+    {
+        public class AdvogadoController
+        {
+            public Object ExecutarOpBD(char _c, Advogado _o)
+            {
+                try
+                {
+                    AdvogadoDAO dao = new AdvogadoDAO();
+                    switch (_c)
+                    {
+                        case 'i':
+                            return dao.Inserir(_o);
+                        case 'd':
+                            return dao.Deletar(_o.Id);
+                        case 'a':
+                            return dao.Atualizar(_o);
+                        case 't':
+                            return dao.ListarTodos();
+                        case 'o':
+                            return dao.BuscarPorID(_o.Id);
+                        default:
+                            return null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
+}
