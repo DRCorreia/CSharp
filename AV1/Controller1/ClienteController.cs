f55af56211627e70e8046c02328010f5e8e652d5
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-13 14:48:59 -0300

    Add files via upload

---
+++ AV1/Controller1/ClienteController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller1
+{
+    class ClienteController
+    {
+        public class ClienteController
+        {
+            public Object ExecutarOpBD(char _c, Cliente _o)
+            {
+                try
+                {
+                    ClienteDAO dao = new ClienteDAO();
+                    switch (_c)
+                    {
+                        case 'i':
+                            return dao.Inserir(_o);
+                        case 'd':
+                            return dao.Deletar(_o.Id);
+                        case 'a':
+                            return dao.Atualizar(_o);
+                        case 't':
+                            return dao.ListarTodos();
+                        case 'o':
+                            return dao.BuscarPorID(_o.Id);
+                        default:
+                            return null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
+}
