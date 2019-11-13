2eca0cd71f3a2416082318c2f848f42124e25a7d
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-13 14:43:14 -0300

    Update ClienteDAO.cs

--- AV1/DAO/ClienteDAO.cs
+++ AV1/DAO/ClienteDAO.cs
@@ -1,12 +1,148 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace DAO
 {
-    class ClienteDAO
+    public class ClienteDAO
     {
+        public Cliente BuscarPorID(Int64 _id)
+        {
+            Cliente c  = null;
+            try
+            {
+                String SQL = String.Format("SELECT * FROM cliente WHERE id = {0} ", _id);
+
+                SqlCeDataReader data = BD.ExecutarSelect(SQL);
+
+                if (data.Read())
+                {
+                    c = new Advogado();
+
+                    c.Id = data.GetInt32(0);
+                    c.Nome = data.GetString(1);
+                    c.Email = data.GetString(2);
+                    c.Telefone = data.GetString(3);
+                }
+
+                data.Close();
+                BD.FecharConexao();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return c;
+        }
+
+        public Boolean InserirBD(Cliente _objeto)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("INSERT INTO cliente (id,nome, email, telefone) VALUES ('{0}', '{1}', '{2}', '{3}')",
+                    _objeto.Id,
+                    _objeto.Nome,
+                    _objeto.Email,
+                    _objeto.Telefone);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public Boolean AlterarBD(Cliente _objeto)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("UPDATE cliente SET email = '{0}', telefone = '{1}' WHERE id = {2};",
+                    _objeto.Email,
+                    _objeto.Telefone,
+                    _objeto.Id);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public Boolean DeletarBD(Int64 _id)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("DELETE FROM cliente WHERE id = {0};", _id);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public List<Cliente> ListarTodos()
+        {
+            List<Cliente> listaAdvogado = new List<Cliente>();
+            try
+            {
+                String SQL = "SELECT * FROM advogado;";
+
+                SqlCeDataReader data = BD.ExecutarSelect(SQL);
+
+                while (data.Read())
+                {
+                   Cliente c = new Cliente();
+
+                    cId = data.GetInt32(0);
+                    c.Nome = data.GetString(1);
+                    c.Email = data.GetString(2);
+                    c.Telefone = data.GetString(3);
+
+                    listaAdvogado.Add(a);
+                }
+
+                data.Close();
+                BD.FecharConexao();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return listaCliente;
+        }
     }
 }
