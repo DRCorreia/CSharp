d3ed32fe61fc3ad586471d9221f76a7ceaefd70d
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-13 15:58:27 -0300

    Add files via upload

---
+++ AV1/View/frmProcurarCliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class frmProcurarCliente : Form
+    {
+        public frmProcurarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            Cliente c = new Cliente();
+            c.Id_Cli = txbID;
+            ClienteController ctrl = new ClienteController();
+            ctrl.ExecutarOpBD('o', a);
+            this.Close();
+        }
+    }
+}
