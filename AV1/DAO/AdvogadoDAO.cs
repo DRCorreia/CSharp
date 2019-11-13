2dc690f7cbd9023e54f7c3a1438212e2ab1e9063
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-13 15:00:03 -0300

    Update AdvogadoDAO.cs

--- AV1/DAO/AdvogadoDAO.cs
+++ AV1/DAO/AdvogadoDAO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,25 +9,23 @@ namespace DAO
 {
     public class AdvogadoDAO
     {
-        public List<Advogado> BuscarAdvogadoPorID(Int32 _idAdvogado)
+        public Advogado BuscarPorID(Int64 _id)
         {
-            List<Advogado> listaAdvogados = new List<Advogado>();
+            Advogado a = null;
             try
             {
-                String SQL = String.Format("SELECT * FROM advogado WHERE id = {0};", _idAdvogado);
+                String SQL = String.Format("SELECT * FROM advogado WHERE id = {0} ", _id);
 
                 SqlCeDataReader data = BD.ExecutarSelect(SQL);
 
-                while (data.Read())
+                if (data.Read())
                 {
-                    Advogado a = new Advogado();
+                    a = new Advogado();
 
-                    a.Id_adv = data.GetInt32(0);
-                    a.Nome_adv = data.GetString(1);
-                    a.Email_adv = data.GetString(2);
-                    a.Tel_adv = data.GetString(3);
-
-                    listaAdvogados.Add(c);
+                    a.Id = data.GetInt32(0);
+                    a.Nome = data.GetString(1);
+                    a.Email = data.GetString(2);
+                    a.Telefone = data.GetString(3);
                 }
 
                 data.Close();
@@ -38,8 +36,114 @@ namespace DAO
                 throw new Exception(ex.Message);
             }
 
-            return listaAdvogados;
+            return a;
         }
 
+        public Boolean InserirBD(Advogado _objeto)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("INSERT INTO advogado (id,nome, email, telefone) VALUES ('{0}', '{1}', '{2}', '{3}')",
+                    _objeto.Id,
+                    _objeto.Nome,
+                    _objeto.Email,
+                    _objeto.Telefone);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public Boolean AlterarBD(Advogado _objeto)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("UPDATE advogado SET email = '{0}', telefone = '{1}' WHERE id = {2};",
+                    _objeto.Email,
+                    _objeto.Telefone,
+                    _objeto.Id);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public Boolean DeletarBD(Int64 _id)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("DELETE FROM advogado WHERE id = {0};", _id);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public List<Advogado> ListarTodos()
+        {
+            List<Advogado> listaAdvogado = new List<Advogado>();
+            try
+            {
+                String SQL = "SELECT * FROM advogado;";
+
+                SqlCeDataReader data = BD.ExecutarSelect(SQL);
+
+                while (data.Read())
+                {
+                    Advogado a = new Advogado();
+
+                    a.Id = data.GetInt32(0);
+                    a.Nome = data.GetString(1);
+                    a.Email = data.GetString(2);
+                    a.Telefone = data.GetString(3);
+
+                    listaAdvogado.Add(a);
+                }
+
+                data.Close();
+                BD.FecharConexao();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return listaAdvogado;
+        }
     }
 }
+
