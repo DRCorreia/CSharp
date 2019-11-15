f5473b54f8a3c305077d78371959cdcae72dd6a6
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-15 12:47:49 -0300

    Add files via upload

---
+++ AV1/Controller1/CausasController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller1
+{
+    public class CausasController
+    {
+        public Object ExecutarOpBD(char _c, CausasFinalizadas _o)
+        {
+            try
+            {
+                CausasFinalizadasDAO dao = new CausasFinalizadasDAO();
+                switch (_c)
+                {
+                    case 'i':
+                        return dao.Inserir(_o);
+                    case 'd':
+                        return dao.Deletar(_o.Id_processo);
+                    case 'a':
+                        return dao.Atualizar(_o);
+                    case 't':
+                        return dao.ListarTodos();
+                    case 'o':
+                        return dao.BuscarPorID(_o.Id_processo);
+                    default:
+                        return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
