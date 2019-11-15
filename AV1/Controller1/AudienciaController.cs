eefe64661df3fa51327b9859ba456cbcd09b0da8
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-15 13:29:30 -0300

    Add files via upload

---
+++ AV1/Controller1/AudienciaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using Model;
+
+namespace Controller1
+{
+        public class AudienciaController
+        {
+            public Object ExecutarOpBD(char _c, Audiencia _o)
+            {
+                try
+                {
+                    AudienciaDAO dao = new AudienciaDAO();
+                    switch (_c)
+                    {
+                        case 'i':
+                            return dao.Inserir(_o);
+                        case 'd':
+                            return dao.Deletar(_o.Id_audiencia);
+                        case 'a':
+                            return dao.Atualizar(_o);
+                        case 't':
+                            return dao.ListarTodos();
+                        case 'o':
+                            return dao.BuscarPorID(_o.Id_audiencia);
+                        default:
+                            return null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+}
