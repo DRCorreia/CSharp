e18e82cd8984045a348fd9d4c15329cc13fa5144
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-15 13:29:49 -0300

    Add files via upload

---
+++ AV1/DAO/AudienciaDAO.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DAO
+{
+    public class AudienciaDAO
+    {
+        public Audiencia BuscarPorID(Int64 _id)
+        {
+            Audiencia a = null;
+            try
+            {
+                String SQL = String.Format("SELECT * FROM audiencia WHERE id_audiencia = {0} ", _id);
+
+                SqlCeDataReader data = BD.ExecutarSelect(SQL);
+
+                if (data.Read())
+                {
+                    a = new Audiencia();
+
+                    a.Id_audiencia = data.GetInt32(0);
+                    a.Nome_adv = data.GetString(1);
+                    a.Nome_cli = data.GetString(2);
+                    a.Id_processo = data.GetInt32(3);
+                    a.Data_audiencia = data.GetDate(4);
+                }
+
+                data.Close();
+                BD.FecharConexao();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return a;
+        }
+
+        public Boolean InserirBD(Audiencia _objeto)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("INSERT INTO audiencia (id_audiencia,nome_adv,nome_cli, id_processo , data_audiencia) VALUES ('{0}', '{1}', '{2}', '{3}','{4}')",
+                    _objeto.Id_audiencia,
+                    _objeto.Nome_adv,
+                    _objeto.Nome_cli,
+                    _objeto.Id_processo,
+                    _objeto.Data_audiencia);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public Boolean AlterarBD(Audiencia _objeto)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("UPDATE audiencia SET data_audiencia = '{0}' WHERE id_audiencia = {1};",
+                    _objeto.Data_audiencia,
+                    _objeto.Id_audiencia);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public Boolean DeletarBD(Int64 _id)
+        {
+            bool resultado = false;
+            try
+            {
+                String SQL = String.Format("DELETE FROM audiencia WHERE id_audiencia = {0};", _id);
+
+                int linhaAfetadas = BD.ExecutarIDU(SQL);
+
+                if (linhaAfetadas > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return resultado;
+        }
+
+        public List<Audiencia> ListarTodos()
+        {
+            List<Audiencia> listaAudiencia = new List<Audiencia>();
+            try
+            {
+                String SQL = "SELECT * FROM audiencia;";
+
+                SqlCeDataReader data = BD.ExecutarSelect(SQL);
+
+                while (data.Read())
+                {
+                    Audiencia a = new Audiencia();
+
+                    a.Id_audiencia = data.GetInt32(0);
+                    a.Nome_adv = data.GetString(1);
+                    a.Nome_cli = data.GetString(2);
+                    a.Id_processo = data.GetInt32(3);
+                    a.Data_audiencia = data.GetDate(4);
+
+                    listaAudiencia.Add(a);
+                }
+
+                data.Close();
+                BD.FecharConexao();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return listaAudiencia;
+        }
+    }
+}
