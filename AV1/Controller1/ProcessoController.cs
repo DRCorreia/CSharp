814bf36063155c46457b8a728f1e75b4e01a6dac
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-15 14:03:29 -0300

    Add files via upload

---
+++ AV1/Controller1/ProcessoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using DAO;
+
+namespace Controller1
+{
+    public class ProcessoController
+    {
+        public Object ExecutarOpBD(char _c, Processo _o)
+        {
+            try
+            {
+                ProcessoDAO dao = new ProcessoDAO();
+                switch (_c)
+                {
+                    case 'i':
+                        return dao.Inserir(_o);
+                    case 'd':
+                        return dao.Deletar(_o.Id_processo);
+                    case 'a':
+                        return dao.Atualizar(_o);
+                    case 't':
+                        return dao.ListarTodos();
+                    case 'o':
+                        return dao.BuscarPorID(_o.Id_processo);
+                    default:
+                        return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
