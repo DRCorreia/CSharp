a476e8ae70789a58f779c378b308b7f32b1a0f47
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-22 16:27:50 -0300

    Update frmExcluirCausaFinalizada.cs

--- AV1/View/frmExcluirCausaFinalizada.cs
+++ AV1/View/frmExcluirCausaFinalizada.cs
@@ -22,7 +22,7 @@ namespace View
         private void btnExcluir_Click(object sender, EventArgs e)
         {
             CausasFinalizadas c = new CausasFinalizadas();
-            c.Id_processo = txbID;
+            c.Processo.Id_processo = txbID;
 
             CausasController ctrl = new CausasController();
             ctrl.ExecutarOpBD('e', c);
